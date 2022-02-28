d26ea130570cdcb71dad5f7cbec9664f049f5f88
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2022-02-27 22:07:53 -0500

    se mejora la solución para ocultar menu y botones default de SCORM

--- maquetaRED/js/hideMenuSCORM.js
+++ maquetaRED/js/hideMenuSCORM.js
@@ -1,7 +1,18 @@
-let menuSCORM = parent.document.getElementById("scorm_toc_toggle");
-if (menuSCORM) {
-    menuSCORM.style.display = "none";
-    parent.document.getElementById("scorm_navpanel").style.display = "none";
-    parent.document.getElementById("scorm_toc").style.display = "none";
-    parent.document.getElementById("scorm_content").style.width = "100%"
+var style = document.createElement('style');
+style.innerHTML = `
+#scorm_toc_toggle,
+#scorm_navpanel,
+#scorm_toc {
+    display: none !important;
 }
+
+#scorm_content {
+    width: 100% !important;
+}
+
+#page-mod-scorm-player #scormpage #scorm_object {
+    width: 100% !important;
+    height: 100% !important;
+}
+`;
+parent.document.head.appendChild(style);
