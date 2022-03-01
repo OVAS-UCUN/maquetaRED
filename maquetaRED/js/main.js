204f9d6efa8e7470020cad306e592fe64df079b7
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2022-03-01 13:36:44 -0500

    Solucion para visualizar nombre con la primer letra en mayuscula

--- maquetaRED/js/main.js
+++ maquetaRED/js/main.js
@@ -1,13 +1,19 @@
+function firstLetterUp(string) {
+    string = string.trim().toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
 function fillFieldUsername() {
     // Busca el campo para llenar con el nombre del usuario
     let userFieldSCORM = document.getElementById('username');
     if (userFieldSCORM) {
         let getUsername = String(scorm.get("cmi.core.student_name"));
         if (getUsername !== "null") {
-            let name = getUsername.split(",")
-            let username = `${name[1]} ${name[0]}`;
-            console.log("La API de SCORM conecta con el nombre del usuario");
-            userFieldSCORM.innerHTML = username.trim();
+            let name = getUsername.split(",");
+            let username = `${firstLetterUp(name[1])} ${firstLetterUp(name[0])}`;
+            console.log(username);
+            userFieldSCORM.innerHTML = username;
         }
     }
 }
